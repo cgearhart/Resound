a8daf4cf52d276ed8f6b9008cc855ec771109cc9
CGearhart <cgearhart@gmail.com>
2014-12-23 20:33:29 -0500

    Rename package to "resound" module

--- resound/tests/test_resound.py
+++ resound/tests/test_resound.py
@@ -1,21 +1,23 @@
 
 import unittest
 
-import wavfile
-import please
+import numpy as np
+
+import resound
 
 
 class PleaseTest(unittest.TestCase):
 
     def setUp(self):
-        _, self.data = wavfile.read('tests/sample.wav')
+        # import sample data from a wav file
+        self.data = np.fromfile('tests/sample.dat', dtype=np.int16)
 
     def test_spectrogram(self):
         """
         spectrogram should return half the frame width + 1 frequency
         components - the 0 component and all positive components
         """
-        specgram = please.spectrogram(self.data, 4096, 2048)
+        specgram = resound.spectrogram(self.data, 4096, 2048)
         self.assertEqual(len(specgram[0, :]), 2049)
 
     def test_fingerprints(self):
@@ -23,8 +25,8 @@ class PleaseTest(unittest.TestCase):
         fingerprint keys should be of type<long> (native python type) for
         compatibility with app engine datastore
         """
-        fpg = please.fingerprints(self.data, 44100)
-        self.assertIsInstance(next(fpg), long)
+        fpg = resound.hashes(self.data, 44100)
+        self.assertIsInstance(next(fpg)[0], long)
 
 
 if __name__ == "__main__":
